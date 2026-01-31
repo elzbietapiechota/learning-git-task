1fa3a220f5879fd3104e4ba3e16ec0a2ca7aa117
Elzbieta Piechota <ela.polakiewicz@gmail.com>
2026-01-31 10:55:28 +0100

    Add total amount of products

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 shopping_list = {
     "piekarnia": ["chleb", "bułki", "pączek"],
     "warzywniak": ["marchew", "seler", "rukola"]
-}
-shopping_list = {
-    "piekarnia": ["chleb", "bułki", "pączek"],
-    "warzywniak": ["marchew", "seler", "rukola"]
-}
 
-
+}
+amount = 0
 for shop, products in shopping_list.items():
     
     shop_title = shop.title()
@@ -15,5 +11,7 @@ for shop, products in shopping_list.items():
 
     print(f"Idę do {shop_title} i kupuję tam {products_title}.") 
     
- 
+    amount = amount + len(products)
+
+print(f"W sumie kupuję {amount} produktów")
 
